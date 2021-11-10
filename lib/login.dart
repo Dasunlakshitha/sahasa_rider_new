f596773d52052feb85a3506f020740d6c2456ecc
Dasun lakshitha <42791160+Dasunlakshitha@users.noreply.github.com>
2021-11-10 20:53:38 +0530

    loginUi

---
+++ lib/login.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:sahasa_rider_new/models/login.dart';
+
+class LogIn extends StatefulWidget {
+  const LogIn({Key key}) : super(key: key);
+
+  @override
+  _LogInState createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  final LoginData loginData = LoginData();
+  TextEditingController username = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white12,
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              _title(),
+              const SizedBox(
+                height: 20,
+              ),
+              _formWidget(),
+              const SizedBox(height: 30),
+              _submitButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _title() {
+    return Image.asset(
+      "assets/images/logo.png",
+      width: MediaQuery.of(context).size.width * 0.7,
+    );
+  }
+
+  Widget _formWidget() {
+    return Form(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        const Text(
+          'Username',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        TextFormField(
+          style: const TextStyle(color: Colors.white),
+          controller: username,
+          validator: (value) {
+            if (value.isEmpty) {
+              // return errorMessage('Username Required.');
+            }
+            return null;
+          },
+          onSaved: (value) {
+            setState(() {
+              loginData.username = value;
+            });
+          },
+          decoration: const InputDecoration(
+              contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+                borderSide: BorderSide(color: Colors.grey, width: 2),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+                borderSide: BorderSide(color: Colors.blue, width: 2),
+              ),
+              fillColor: Colors.black,
+              hintStyle: TextStyle(fontSize: 15, color: Colors.white70),
+              hintText: 'Username'),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        const Text(
+          'Password',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(height: 5),
+        TextFormField(
+          style: const TextStyle(color: Colors.white),
+          controller: password,
+          obscureText: true,
+          validator: (value) {
+            if (value.isEmpty) {
+              // return errorMessage('Password Required.');
+            }
+            return null;
+          },
+          onSaved: (value) {
+            setState(() {
+              loginData.password = value;
+            });
+          },
+          decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 15),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+              borderSide: BorderSide(color: Colors.grey, width: 2),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+              borderSide: BorderSide(color: Colors.blue, width: 2),
+            ),
+            fillColor: Colors.black,
+            hintStyle: TextStyle(fontSize: 15, color: Colors.white70),
+            hintText: 'Password',
+            //suffixIcon: IconButton(onPressed:(){},icon: Icon(Icons.visibility_off)),
+          ),
+        ),
+      ],
+    ));
+  }
+
+  Widget _submitButton() {
+    return InkWell(
+      onTap: () {
+        //_login();
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width / 2.4,
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Color(0xff206BE2), Color(0xff206BE2)])),
+        // child: loading
+        //     ? const CircularProgressIndicator()
+        //    : const Text(
+        child: const Text(
+          'Login',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, fontSize: 25, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models/user.dart
@@ -0,0 +1,91 @@
+class Users {
+  bool done;
+  Body body;
+  String message;
+
+  Users({this.done, this.body, this.message});
+
+  Users.fromJson(Map<String, dynamic> json) {
+    done = json['done'];
+    if (json['body'] != null) {
+      body = Body.fromJson(json['body']);
+    }
+    message = json['message'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['done'] = done;
+    if (body != null) {
+      data['body'] = body.toJson();
+    }
+    data['message'] = message;
+    return data;
+  }
+}
+
+class Body {
+  User user;
+
+  Body({this.user});
+
+  Body.fromJson(Map<String, dynamic> json) {
+    if (json['user'] != null) {
+      user = User.fromJson(json['user']);
+    }
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    if (user != null) {
+      data['user'] = user.toJson();
+    }
+    return data;
+  }
+}
+
+class User {
+  var id;
+  var username;
+  var isAvailable;
+  var nic;
+  var name;
+  var contactNo;
+  var vehicleNumber;
+  var accountId;
+
+  User({
+    this.id,
+    this.username,
+    this.isAvailable,
+    this.nic,
+    this.name,
+    this.contactNo,
+    this.vehicleNumber,
+    this.accountId,
+  });
+
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    username = json['username'];
+    isAvailable = json['is_available'];
+    nic = json['nic'];
+    name = json['name'];
+    contactNo = json['contact_no'];
+    vehicleNumber = json['vehicle_number'];
+    accountId = json['account_id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['username'] = username;
+    data['is_available'] = isAvailable;
+    data['nic'] = nic;
+    data['name'] = name;
+    data['contact_no'] = contactNo;
+    data['vehicle_number'] = vehicleNumber;
+    data['account_id'] = accountId;
+    return data;
+  }
+}
